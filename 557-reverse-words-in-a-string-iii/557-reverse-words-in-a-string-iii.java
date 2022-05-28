270844cc063eb9265f5a0f1c863f48b9fd0615bf
BodhiYonzon <87485437+BodhiYonzon@users.noreply.github.com>
2022-05-28 16:27:30 +0530

    Time: 182 ms (6.13%), Space: 118.1 MB (7.49%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -3,7 +3,7 @@ class Solution {
         String t="",r="";
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==' '){
-                t=t+r+" ";
+                t+=r+' ';
                 r="";
             }
             else
